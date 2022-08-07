ba98178c61e8d6473024cf85adb3ab47ff46c6d7
Kim <kj@kjsoft.dk>
2022-08-07 08:45:52 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { Box, Slider } from '@mui/material';
+import { Container } from '@mui/system';
+
+import { useState,useEffect } from 'react';
 
 function App() {
+
+  const [size,setSize]=useState(500)
+  const [source, setSource]=useState(["1.jpeg","2.jpeg","3.jpeg"])
+  const [index, setIndex]=useState(0)
+  const changeSize=(x:any)=>{
+    console.log(x.target.value)
+    setSize(x.target.value)
+    }
+    
+    useEffect(()=>{
+      startTime(5000)
+    })
+
+  const startTime=(timerValue:number)=>{
+
+    setTimeout(()=>{
+      let maxvalue=source.length-1;
+
+      if(index===maxvalue)
+      {
+        setIndex(0)
+      }else{
+        setIndex(index+1);
+      }
+      },timerValue)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <body>
+      <Container maxWidth="sm">
+        <Slider   defaultValue={size} max={800} onChange={e=>changeSize(e)} aria-label="Disabled slider" />
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          minHeight="50vh"
         >
-          Learn React
-        </a>
-      </header>
+          <img style={{'height': '100vh' }} src={source[index]}></img>
+         
+        </Box>
+      </Container>
+      </body>
     </div>
   );
 }
+
+
 
 export default App;
